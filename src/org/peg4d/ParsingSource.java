25c4038fa12db8a9e03132b9b4532f311080b730
uchida <uchida.atsushi.8@gmail.com>
2015-01-05 01:52:08 +0900

    fix bugs

--- src/org/peg4d/ParsingSource.java
+++ src/org/peg4d/ParsingSource.java
@@ -516,8 +516,11 @@ class FileSource extends ParsingSource {
 		int count = 0;
 		int i = offset;
 		while(this.buffer[i] != '\n') {
-				i--;
-				count++;
+			i--;
+			count++;
+			if(i < 0) {
+				break;
+			}
 		}
 		return count;
 	}
